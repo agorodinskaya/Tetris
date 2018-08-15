590270ac01e06dbc1aee54519e3e254fb9148a76
agorodinskaya <agorodinskaya@gmail.com>
2018-08-15 15:35:08 +1000

    figure colliding 🌈

--- js/main.js
+++ js/main.js
@@ -7,6 +7,18 @@ ctx.scale(20, 20);
 // drawing T, additional row on the top to define the center of rotation:
 const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
 
+const collide = (arena, player) => {
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
 const createMatrix = (w, h) => {
   const matrix = [];
   while (h--) {
@@ -33,13 +45,24 @@ const drawMatrix = (matrix, offset) => {
     });
   });
 };
+
+//copy all the values from player into arena:
 const merge = (arena, player) => {
   player.matrix.forEach((row, y) => {
-    row.forEach((value, x) => {});
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
   });
 };
 const playerDrop = () => {
   player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
   dropCounter = 0;
 };
 
